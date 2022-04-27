f0dec45f272213bad52754292470578bbe2c1d4c
SupriyaSudini <95210394+SupriyaSudini@users.noreply.github.com>
2022-04-27 22:46:27 +0530

    Update ToppingOption.js

--- src/pages/entry/ToppingOption.js
+++ src/pages/entry/ToppingOption.js
@@ -6,7 +6,7 @@ export default function ToppingOptions({name, imagePath}){
         <Col xs={12} sm={6} md={4} lg={3} style={{textAlign:"center"}}>
           <img 
             style ={{width:"75%"}} 
-            src={`http://localhost3030/${imagePath}`} 
+            src={`http://localhost:3030/${imagePath}`} 
             alt={`${name} topping`} />
         </Col>
     );
