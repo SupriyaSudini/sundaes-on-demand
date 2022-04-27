7db245d575fa13511c449b94d9317a36203f44a6
SupriyaSudini <95210394+SupriyaSudini@users.noreply.github.com>
2022-04-27 22:42:27 +0530

    Update handlers.js

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,21 +1,21 @@
 import { rest } from "msw";
 
 export const handlers = [
-  rest.get("http://localhost:3030/scoops", (res, req, ctx) => {
+  rest.get('http://localhost:3030/scoops', (res, req, ctx) => {
     return res(
       ctx.json([
-        { name: "Chocolate", imagePath: "/images/chocolate.png" },
-        { name: "Vanilla", imagePath: "/images/vanilla.png" },
+        { name: "Chocolate", imagePath: '/images/chocolate.png' },
+        { name: "Vanilla", imagePath: '/images/vanilla.png' },
       ])
     );
   }),
 
-  rest.get("http://localhost:3030/toppings", (res, req, ctx) => {
+  rest.get('http://localhost:3030/toppings', (res, req, ctx) => {
     return res(
       ctx.json([
-        { name: "Cherries", imagePath: "/images/cherries.png" },
-        { name: "M&Ms", imagePath: "/images/m&ms.png" },
-        { name: "Hot fudge", imagePath: "/images/hot-fudge.png" },
+        { name: "Cherries", imagePath: '/images/cherries.png' },
+        { name: "M&Ms", imagePath: '/images/m&ms.png' },
+        { name: "Hot fudge", imagePath: '/images/hot-fudge.png' },
       ])
     );
   }),
